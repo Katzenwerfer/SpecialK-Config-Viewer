e2b2efcf4e64b98af4f0300a24367ae683dcc340
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-03-22 16:52:09 -0600

    Handle "Keybind" entries

--- main.ps1
+++ main.ps1
@@ -7,10 +7,19 @@ $content = Get-Content -Path '.\.ext\config.cpp' -Raw
 
 $trimmedContent = $content | Select-String -Pattern 'static const std::initializer_list <param_decl_s> params_to_build[\s\S]+for \( auto& decl : params_to_build \)'
 
-$configEntries = $trimmedContent.Matches.Value | Select-String -Pattern '(ConfigEntry\s*\([\s\S]+?(?=\),)\),(?=\r\n)' -AllMatches
+$configEntries = $trimmedContent.Matches.Value | Select-String -Pattern '(?:ConfigEntry|Keybind)\s*\([\s\S]+?(?=\),)\),(?=\r\n)' -AllMatches
 
 $normalizedEntries = foreach ($entry in $configEntries.Matches.Value) {
     if ($entry -match 'ConfigEntry\s*\(([^,]+),\s*L"([\s\S]+?(?=" ?,)" ?,)\s*([^,]+),\s*L"([^"]+)",\s*L"([^"]+)"\),') {
+        [PSCustomObject]@{
+            Parameter   = $Matches[1] -replace '([^\s]+)(?:\s*([^\s]+))?', '$1$2'
+            Description = $Matches[2] -replace '([^"]+)"(?:\s*L"([^"]+)")?(?:\s*L"([^"]+)")?,', '$1$2$3'
+            IniFile     = $Matches[3]
+            Section     = $Matches[4]
+            Key         = $Matches[5]
+        }
+    }
+    elseif ($entry -match 'Keybind\s*\( ?&([^,]+),\s*L"([\s\S]+?(?=" ?,)" ?,)\s*([^,]+),\s*L"([^"]+)"\),') {
         [PSCustomObject]@{
             Parameter   = $Matches[1] -replace '([^\s]+)(?:\s*([^\s]+))?', '$1$2'
             Description = $Matches[2] -replace '([^"]+)"(?:\s*L"([^"]+)")?(?:\s*L"([^"]+)")?,', '$1$2$3'
