15511bdf74ffa253a476bd45cfbb4b20286f6464
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2026-03-22 00:51:25 -0600

    Store config entries in variable

--- main.ps1
+++ main.ps1
@@ -4,3 +4,19 @@ if (-not (Test-Path -Path '.\.ext\config.cpp')) {
 }
 
 $content = Get-Content -Path '.\.ext\config.cpp'
+
+$configEntries = $content | Where-Object {
+    $PSItem -match '^\s*ConfigEntry'
+}
+
+$results = foreach ($entry in $configEntries) {
+    if ($entry -match 'ConfigEntry\s*\(([^,]+),\s*L"([^"]+)",\s*([^,]+),\s*L"([^"]+)",\s*L"([^"]+)"\)') {
+        [PSCustomObject]@{
+            Parameter   = $Matches[1]
+            Description = $Matches[2]
+            IniFile     = $Matches[3]
+            Section     = $Matches[4]
+            Key         = $Matches[5]
+        }
+    }
+}
